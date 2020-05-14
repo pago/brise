2c3a23d801a6f0bfbf093268cd517f207591d970
Patrick Gotthardt <patrick.gotthardt@trivago.com>
2020-05-14 10:27:03 +0200

    Add test for replacing the chosen tag through the `as` property

--- test/tw.test.tsx
+++ test/tw.test.tsx
@@ -2,14 +2,39 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { tw } from '../src';
 
-describe('it', () => {
+describe('tw', () => {
+  let container: HTMLElement;
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(container);
+  });
+
+  const render = (Component: React.ReactElement) => {
+    ReactDOM.render(Component, container);
+  };
+
+  const findBySelector = (query: string) => {
+    return container.querySelector(query);
+  };
+
   it('renders with classes', () => {
-    const div = document.createElement('div');
     const Headline = tw.h1<any>`text-3xl`;
-    ReactDOM.render(<Headline />, div);
-    const elem = div.querySelector('h1');
+    render(<Headline />);
+
+    const elem = findBySelector('h1');
     expect(elem).toBeDefined();
     expect(elem!.className).toEqual('text-3xl');
-    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('allows changing the tag', () => {
+    const Headline = tw.h1<any>`text-2xl`;
+    render(<Headline as="h2" />);
+
+    const elem = findBySelector('h2');
+    expect(findBySelector('h1')).toBeNull();
+    expect(elem).toBeDefined();
+    expect(elem!.classList.contains('text-2xl')).toBeTruthy();
   });
 });
